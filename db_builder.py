344043bc371c6226de7e91a0321775c8d6b93ef2
aisandovalm <a.sandoval.magnani@gmail.com>
2020-07-12 18:54:02 -0400

    working OK - v1

--- db_builder.py
+++ db_builder.py
@@ -1,18 +1,21 @@
 import cv2
 import os
+import time
 
 class DBBuilder:
-    def __init__(self, dir_path, images_path='./images', ann_filename='annotations.txt', overwrite=False, plates_delimiter=' | '):
+    def __init__(self, dir_path, images_path='images', ann_filename='annotations.txt', overwrite=False, plates_delimiter=' | '):
         self.file = open(os.path.join(dir_path, ann_filename), 'a')
         self.plates_delimiter = plates_delimiter
-        self.image_path = images_path
+        self.images_path = os.path.join(dir_path, images_path)
 
-
-    def write_line(self, image, image_path, bboxes, nplates):
+    def write_line(self, image, bboxes, nplates):
+        current_time = time.strftime("%Y%m%d-%H%M%S")
+        image_path = os.path.join(self.images_path, str(current_time) + ".png")
         line = image_path + ' '
         
         first_plate = True
         for bbox, nplate in zip(bboxes, nplates):
+            print(bbox)
             if first_plate:
                 line += ' {} {} {} {} {}'.format(bbox[0], bbox[1], bbox[2], bbox[3], nplate)
                 first_plate = False
@@ -20,11 +23,10 @@ class DBBuilder:
                 line += '{}{} {} {} {} {}'.format(self.plates_delimiter, bbox[0], bbox[1], bbox[2], bbox[3], nplate)
 
         line = line+'\n'
-        print('writing: {}'.format(line))
         self.file.write(line)
 
         self.save_image(image, image_path)
 
     def save_image(self, image, image_path):
-        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        cv2.imwrite(image_path, image_rgb)
+        #image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        cv2.imwrite(image_path, image)
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import os, sys
 import glob
 import threading
 from threading import Timer
+import numpy as np
 
 DIR_SOURCE_PATH = 'D:\Development\Big Data and Machine Learning\Patentes\dataset-gopro'
 DIR_VIDEOS_PATH = os.path.join(DIR_SOURCE_PATH, 'videos')
@@ -23,7 +24,7 @@ def prepare_videos(source):
         VIDEOS_LIST.append(video)
 
 def run_annotator():
-    global prepareVideosThread, _DELAY, VIDEOS_LIST
+    global prepareVideosThread, _DELAY, VIDEOS_LIST, dbBuilder
 
     try:
         video_path = VIDEOS_LIST.pop(0)
@@ -39,20 +40,23 @@ def run_annotator():
                 key = cv2.waitKey(_DELAY) & 0xff
                 if key == 97: # a = make annotation
                     bboxes = cv2.selectROIs("Annotator", frame)
-                    frame_bkp = frame
-                    print('boxes: {}'.format(bboxes))
+                    frame_bkp = np.copy(frame)
 
-                    # ask for number of each box
+                    # draw boxes
                     for i, box in enumerate(bboxes):
                         p1 = (box[0], box[1])
                         p2 = (box[0]+box[2], box[1]+box[3])
                         cv2.rectangle(frame_bkp, p1, p2, (0,0,255))
-                        cv2.putText(frame_bkp, 'lp {}'.format(i), p1, cv2.FONT_HERSHEY_COMPLEX, 1, (0,0,255))
-
+                        cv2.putText(frame_bkp, 'n {}'.format(i), p1, cv2.FONT_HERSHEY_COMPLEX, 0.5, (0,0,255))
                     cv2.imshow("Annotator", frame_bkp)
+                    key = cv2.waitKey(1)
+                    
+                    lpNumbers = []
                     for i, box in enumerate(bboxes):
                         lp_number = input('License plate number of lp={}: '.format(i))
-                        print('lp_number: {}'.format(lp_number))
+                        lpNumbers.append(lp_number.upper())
+                    
+                    dbBuilder.write_line(frame, bboxes, lpNumbers)
                 if key == 42: # *
                     _DELAY = _DEFAULT_DELAY
                     print(_DELAY)
@@ -64,28 +68,34 @@ def run_annotator():
                     delta = _DEFAULT_DELAY*0.1
                     _DELAY = int(_DELAY+delta)
                     print(_DELAY)
+                if key == 113: # q
+                    return
             else:
                 print('End of video')
                 break
     except IndexError:
-        print('List is empty')
+        #print('List is empty')
+        pass
     finally:
         if (prepareVideosThread is not None) and (not prepareVideosThread.isAlive()):
-            print('Thread is not running. Closing process')
+            #print('Thread is not running. Closing process')
+            return
+        elif prepareVideosThread is None:
             return
         else:
             wait_time = 10
-            print('Thread is running. Waiting {} seconds for the next video'.format(wait_time))
+            #print('Thread is running. Waiting {} seconds for the next video'.format(wait_time))
             Timer(wait_time, run_annotator).start()
 
 def generate_list():
     #VIDEOS_LIST.append('D:\\fiscalia\\video_test\\kauel.mp4')
-    VIDEOS_LIST.append('D:\\fiscalia\\test.mp4')
+    #VIDEOS_LIST.append('D:\\Development\\Big Data and Machine Learning\\Patentes\\dataset-gopro\\videos\\GOPR1310.MP4')
+    VIDEOS_LIST.append('.\\tmp\\20200710-163416.avi')
 
 if __name__ == "__main__":
-    global prepareVideosThread
-    PREPARE_VIDEOS = False
-    #dbBuilder = DBBuilder(DIR_SOURCE_PATH, DIR_IMAGES_PATH)
+    global prepareVideosThread, dbBuilder
+    PREPARE_VIDEOS = True
+    dbBuilder = DBBuilder(DIR_SOURCE_PATH, DIR_IMAGES_PATH)
 
     if PREPARE_VIDEOS:
         prepareVideosThread = threading.Thread(target=prepare_videos, args=(DIR_VIDEOS_PATH,))
@@ -96,5 +106,5 @@ if __name__ == "__main__":
 
     run_annotator()
 
-    print('Closing app')
+    #print('Closing app')
     sys.exit()
